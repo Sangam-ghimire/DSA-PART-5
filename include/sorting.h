89e38cbdc5f490fc646b299b3e15b646fb3fc800
Sangam Ghimire <1sangamghimire1@gmail.com>
2022-11-13 09:54:17 +0545

    added arrays

--- include/sorting.h
+++ include/sorting.h
@@ -7,6 +7,18 @@ private:
 public:
     sorting(/* args */);
     ~sorting();
+
+    int arrayi[100];
+    int arrayq[100];
+
+    int arrayi1[500];
+    int arrayq1[500];
+
+    int arrayi2[1000];
+    int arrayq2[1000];
+
+    void insertionSort();
+
 };
 
 
