60aaa8ffbfc57d28a5e2f21475d7d4b6b81bff64
Sangam Ghimire <1sangamghimire1@gmail.com>
2022-11-27 21:16:53 +0545

    added the options for data size via command line

--- main.cpp
+++ main.cpp
@@ -11,8 +11,10 @@ int main()
     int temp, option = 0;
     int seed = time(0);
     srand(seed);
+    std::cout << "Choose Time \n1.For One lakh data \n2.For Two lakh data \n3.For Three lakh data \n4.For Four lakh data \n5)For Six lakh data \n6)For Five lakh data 7)Enter any number 'n' for 'n' lakh data\n ->";
+    std::cin >> option;
 
-    for (int i = 0; i < 100000; i++)
+    for (int i = 0; i < option * 100000; i++)
     {
         temp = rand();
         sort.vect.push_back(temp);
@@ -21,9 +23,9 @@ int main()
     time(&start);
     sort.insertionSort();
     time(&ending);
-
-    // for (int x : sort.vect)
-    //     cout << x << " ";
+    // to print data
+    //  for (int x : sort.vect)
+    //      cout << x << " ";
 
     cout << "\nTotal time required = "
          << difftime(ending, start)
